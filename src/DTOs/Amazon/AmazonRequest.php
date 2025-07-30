a8d47c50718ea4802c282e32d638206366ec2403
Quentin Schmick <qschmick@gmail.com>
2025-07-30 12:14:20 -0400

    Fixing parse option on amazon request

--- src/DTOs/Amazon/AmazonRequest.php
+++ src/DTOs/Amazon/AmazonRequest.php
@@ -15,7 +15,8 @@ class AmazonRequest extends Data
         public readonly string $url,
         #[WithCast(EnumCast::class)]
         public readonly ?RenderOption $render = null,
-        public readonly ?array $parse = null,
+        public readonly ?bool $parse = null,
+        public readonly ?string $geo_location = null,
         public readonly ?int $pages = null,
         public readonly ?int $start_page = null,
         public readonly ?array $context = null
@@ -28,6 +29,7 @@ class AmazonRequest extends Data
             'domain' => $this->domain,
             'url' => $this->url,
             'parse' => $this->parse,
+            'geo_location' => $this->geo_location,
             'context' => $this->context,
             'pages' => $this->pages,
             'render' => $this->render,

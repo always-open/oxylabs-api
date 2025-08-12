3b989c19999bddb5f182b54c1b6242da2076b857
Quentin Schmick <qschmick@gmail.com>
2025-08-11 23:07:41 -0400

    Adding additional header values

--- src/DTOs/UniversalResultRequestHeader.php
+++ src/DTOs/UniversalResultRequestHeader.php
@@ -8,11 +8,15 @@ class UniversalResultRequestHeader extends Data
 {
     public function __construct(
         public readonly ?string $accept = null,
+        public readonly ?string $te = null,
+        public readonly ?string $referer = null,
         public readonly ?string $user_agent = null,
         public readonly ?string $sec_fetch_dest = null,
         public readonly ?string $sec_fetch_mode = null,
         public readonly ?string $sec_fetch_site = null,
+        public readonly ?string $sec_fetch_user = null,
         public readonly ?string $accept_encoding = null,
         public readonly ?string $accept_language = null,
+        public readonly ?string $upgrade_insecure_requests = null,
     ) {}
 }

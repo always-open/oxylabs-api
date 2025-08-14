28870ec672ddf38a0f746fe4115df5e562bdc6ba
Quentin Schmick <qschmick@gmail.com>
2025-08-14 17:19:44 -0400

    Fixing GS pricing optional field

--- src/DTOs/Google/GoogleShoppingPricingOffer.php
+++ src/DTOs/Google/GoogleShoppingPricingOffer.php
@@ -8,12 +8,12 @@ class GoogleShoppingPricingOffer extends Data
 {
     public function __construct(
         public readonly float $price,
-        public readonly string $seller,
         public readonly string $details,
         public readonly string $currency,
         public readonly string $condition,
         public readonly string $seller_link,
         public readonly float|string $price_shipping,
+        public readonly ?string $seller = null,
         public readonly ?float $price_tax = null,
         public readonly ?float $price_total = null,
     ) {}

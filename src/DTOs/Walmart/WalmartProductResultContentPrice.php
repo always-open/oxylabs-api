d54e88f1640d4317def1d0924a86b76976e374a7
Quentin Schmick <qschmick@gmail.com>
2025-09-02 15:50:55 -0400

    Handling walmart pricing

--- src/DTOs/Walmart/WalmartProductResultContentPrice.php
+++ src/DTOs/Walmart/WalmartProductResultContentPrice.php
@@ -7,8 +7,8 @@ use Spatie\LaravelData\Data;
 class WalmartProductResultContentPrice extends Data
 {
     public function __construct(
-        public readonly float $price,
-        public readonly string $currency,
+        public readonly ?float $price = null,
+        public readonly ?string $currency = null,
         public readonly null|string|float $price_strikethrough = null,
     ) {}
 }

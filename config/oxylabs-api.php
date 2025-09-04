e8ed208bdc5d18d7a41c151acf05b687b1b4f7c6
Quentin Schmick <qschmick@gmail.com>
2025-09-04 10:41:45 -0400

    Removed exit statement

--- config/oxylabs-api.php
+++ config/oxylabs-api.php
@@ -1,7 +1,5 @@
 <?php
 
-die(var_dump(env('OXYLABS_API_REQUEST_LOGGING_ENABLED', true)));
-
 return [
     'base_url' => env('OXYLAB_API_BASE_URL', 'https://data.oxylabs.io/v1/'), // https://realtime.oxylabs.io/v1
     'username' => env('OXYLAB_API_USERNAME'),

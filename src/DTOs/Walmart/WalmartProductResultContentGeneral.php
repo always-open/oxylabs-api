fc6814b3c959764351cb16d528b5df16741d7367
Quentin Schmick <qschmick@gmail.com>
2025-09-03 22:00:20 -0400

    Allowing nulls

--- src/DTOs/Walmart/WalmartProductResultContentGeneral.php
+++ src/DTOs/Walmart/WalmartProductResultContentGeneral.php
@@ -7,11 +7,11 @@ use Spatie\LaravelData\Data;
 class WalmartProductResultContentGeneral extends Data
 {
     public function __construct(
-        public readonly string $url,
-        public readonly array $meta,
-        public readonly string $brand,
-        public readonly string $title,
-        public readonly string $description,
+        public readonly ?string $url = null,
+        public readonly ?array $meta = null,
+        public readonly ?string $brand = null,
+        public readonly ?string $title = null,
+        public readonly ?string $description = null,
         public readonly ?array $images = null,
         public readonly ?string $main_image = null,
         public readonly ?array $badge = null,

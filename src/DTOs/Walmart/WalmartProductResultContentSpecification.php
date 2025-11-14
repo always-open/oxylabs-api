15830036d23b3f9eea80b466dd1166d914b1fbce
Quentin Schmick <qschmick@gmail.com>
2025-11-14 08:01:07 -0500

    Allowing WalmartProductResultContentSpecification to contain nulls

--- src/DTOs/Walmart/WalmartProductResultContentSpecification.php
+++ src/DTOs/Walmart/WalmartProductResultContentSpecification.php
@@ -7,7 +7,7 @@ use Spatie\LaravelData\Data;
 class WalmartProductResultContentSpecification extends Data
 {
     public function __construct(
-        public readonly string $key,
-        public readonly string $value,
+        public readonly ?string $key = null,
+        public readonly ?string $value = null,
     ) {}
 }

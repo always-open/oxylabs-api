7bba18e5f690d3a6c4ac00a276093ffc11b63d40
Quentin Schmick <qschmick@gmail.com>
2026-01-20 13:30:56 -0500

    Added custom toArray for instruction

--- src/DTOs/BrowserInstruction.php
+++ src/DTOs/BrowserInstruction.php
@@ -16,4 +16,19 @@ class BrowserInstruction extends Data
         public readonly ?int $y = null,
         public readonly ?string $filter = null,
     ) {}
+
+
+    public function toArray(): array
+    {
+        return array_filter([
+            'type' => $this->type,
+            'selector' => $this->selector?->toArray(),
+            'wait_time_s' => $this->wait_time_s,
+            'timeout_s' => $this->timeout_s,
+            'on_error' => $this->on_error,
+            'x' => $this->x,
+            'y' => $this->y,
+            'filter' => $this->filter,
+        ]);
+    }
 }

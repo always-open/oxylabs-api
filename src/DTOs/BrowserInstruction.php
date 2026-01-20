06115c5bfc6c621704298cc22b2769fab8945dd9
Quentin Schmick <qschmick@gmail.com>
2026-01-20 12:38:26 -0500

    Fixed typo as it is singular not collection

--- src/DTOs/BrowserInstruction.php
+++ src/DTOs/BrowserInstruction.php
@@ -2,16 +2,13 @@
 
 namespace AlwaysOpen\OxylabsApi\DTOs;
 
-use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Data;
 
 class BrowserInstruction extends Data
 {
     public function __construct(
         public readonly string $type,
-        /* @var BrowserInstructionSelector[] $selector */
-        #[DataCollectionOf(BrowserInstructionSelector::class)]
-        public readonly ?array $selector = null,
+        public readonly ?BrowserInstructionSelector $selector = null,
         public readonly ?int $wait_time_s = null,
         public readonly ?int $timeout_s = null,
         public readonly ?string $on_error = null,
